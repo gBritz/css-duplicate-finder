fead19bb80762977cdee7c455df3beac1956ec10
gBritz <gbritzv@gmail.com>
2015-07-25 16:39:53 -0300

    Adding utility extensions

---
+++ src/CssDupFinder/Extensions/DictionaryExtension.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace CssDupFinder.Extensions
+{
+    public static class DictionaryExtension
+    {
+        public static TResult GetOrNew<TKey, TResult>(this IDictionary<TKey, TResult> dic, TKey key) where TResult : new()
+        {
+            dic.ThrowIfNull("dic");
+
+            TResult value;
+
+            if (dic.ContainsKey(key))
+            {
+                value = dic[key];
+            }
+            else
+            {
+                value = new TResult();
+                dic.Add(key, value);
+            }
+
+            return value;
+        }
+
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> dic, TKey key, TValue value)
+        {
+            dic.ThrowIfNull("dic");
+
+            if (dic.ContainsKey(key))
+            {
+                dic[key] = value;
+            }
+            else
+            {
+                dic.Add(key, value);
+            }
+        }
+    }
+}
---
+++ src/CssDupFinder/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CssDupFinder.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static void ThrowIfNull(this Object value, String parameterName)
+        {
+            if (value == null)
+                throw new ArgumentNullException(parameterName);
+        }
+    }
+}
