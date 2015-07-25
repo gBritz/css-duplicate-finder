006ec8165294a6e6c384347d63dc53b788cfe27a
gBritz <gbritzv@gmail.com>
2015-07-24 23:30:22 -0300

    Create project

---
+++ src/CssDupFinder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CssDupFinder
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
