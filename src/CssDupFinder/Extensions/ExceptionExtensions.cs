94ca1f3746b3906f14e0edc5a5b3d3023f15065f
gBritz <gbritzv@gmail.com>
2015-07-26 11:56:56 -0300

    feat(ExceptionExtensions): ThrowIfNull for String.

--- src/CssDupFinder/Extensions/ExceptionExtensions.cs
+++ src/CssDupFinder/Extensions/ExceptionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CssDupFinder.Extensions
 {
@@ -8,6 +7,12 @@ namespace CssDupFinder.Extensions
         public static void ThrowIfNull(this Object value, String parameterName)
         {
             if (value == null)
+                throw new ArgumentNullException(parameterName);
+        }
+
+        public static void ThrowIfNull(this String value, String parameterName)
+        {
+            if (String.IsNullOrEmpty(value))
                 throw new ArgumentNullException(parameterName);
         }
     }
